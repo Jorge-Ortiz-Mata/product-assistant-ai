cbb9306f85dab64fb47df4ad7a7d95be10984348
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2026-03-21 18:41:50 +0000

    update workflows

--- product-assistant-frontend/src/services/api_service.ts
+++ product-assistant-frontend/src/services/api_service.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://product-assistant-backend-latest.onrender.com/api/v1",
+  baseURL: "https://product-assistant-backend-28604544957.us-central1.run.app/api/v1",
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json"
   }
-})
+});
