a0a0eaa868fae2c20cc5b6c02901dc3ad2588dce
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2026-03-23 21:42:12 +0000

    update

--- product-assistant-frontend/src/services/api_service.ts
+++ product-assistant-frontend/src/services/api_service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://d3ff-2806-2f0-64a1-d7b6-451f-fb3e-79ff-f28.ngrok-free.app/api/v1",
+  baseURL: "https://product-assistant-backend-28604544957.us-central1.run.app/api/v1",
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json"
